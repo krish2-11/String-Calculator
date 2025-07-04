899ba96e473f44391039e7329e5596780a9b242f
Krish <goswamikrish.211@gmail.com>
2025-07-04 22:48:44 +0530

    Add: Return 0 for empty string input

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -4,4 +4,10 @@ public class StringCalculator {
     public static void main(String[] args) {
 
     }
+
+    int add(String number){
+        if(number.isEmpty()) return 0;              // Handling cases for empty string
+
+        return 0;
+    }
 }
--- src/test/java/com/incubyte/StringCalculatorTest.java
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -1,4 +1,32 @@
 package com.incubyte;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+
+    private StringCalculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Setting up a new StringCalculator instance...");
+        calculator = new StringCalculator();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Test finished. Cleaning up...");
+        calculator = null;
+    }
+
+    @Test
+    public void should_returnZero_when_inputIsEmpty() {
+
+        // Getting result from add() method with empty string
+        int result = calculator.add("");
+
+        // Test for empty string output
+        assertEquals(0, result, "Empty string should return 0");
+    }
 }
